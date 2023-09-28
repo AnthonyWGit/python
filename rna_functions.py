6683ae6192cbe3a2459d0e70d08c1095e4b2279b
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-28 20:49:39 +0200

    added complementary rna

--- rna_functions.py
+++ rna_functions.py
@@ -1,20 +1,6 @@
 import random
 
-class RNA:
-    def generate_rna(self):
-        #The array of letters to choose from
-        letters = ['A', 'C', 'G', 'U']
-        #Initilization
-        i = 0
-        #this var contains empty string
-        random_rna=""
-        #
-        while i < 30:
-            random_letter = random.choice(letters)
-            random_rna += random_letter
-            i += 1
-        return random_rna
-    
+class RNA:    
     def generate_rna_new(self, generated_dna):
         dic = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'} #We will do like complementary DNA but replace all Ts with U 
         toTransform = generated_dna
@@ -22,3 +8,10 @@ class RNA:
             toTransform = toTransform.replace(toTransform[i], dic[toTransform[i]])
             toTransform2 = toTransform.replace('T', 'U') #replace all Ts with U
         return toTransform2
+    
+    def generate_rna_com(self, generated_rna):
+        dic = {'A': 'U', 'C': 'G', 'G': 'C', 'U': 'A'} #We will do like complementary DNA but replace all Ts with U 
+        toTransform = generated_rna
+        for i in range(len(toTransform)):
+            toTransform = toTransform.replace(toTransform[i], dic[toTransform[i]])
+        return toTransform
