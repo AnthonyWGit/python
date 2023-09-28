7561c7cd5c345b2848134521438bba8223b1e1e8
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-28 20:48:48 +0200

    dna comp change b/c previous transform all string

--- dna_functions.py
+++ dna_functions.py
@@ -17,8 +17,9 @@ class DNA:
     
     def generate_complementary(self, generated_dna):
         dic = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
+        complementary_dna = ""
         toTransform = generated_dna
-        for i in range(len(toTransform)):
-            toTransform = toTransform.replace(toTransform[i], dic[toTransform[i]])
-        return toTransform
+        for base in generated_dna: # for i as there as as many letters to transform 
+            complementary_dna += dic[base]
+        return complementary_dna
     
